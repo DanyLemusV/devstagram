2854b52e8fe22001302f0c4428e8d7f836b9c22b
DanyLemusV <danylemusvillanueva@gmail.com>
2024-07-20 12:21:21 -0600

    Sección 9, video 49

--- resources/views/nosotros.blade.php
+++ resources/views/nosotros.blade.php
@@ -1,21 +1,8 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
+@section('titulo')
+Nosotros
+@endsection
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-    </head>
-    <body class="font-sans antialiased dark:bg-black dark:text-white/50">
-        <nav>
-            <a href="/">Principal</a>
-            <a href="/nosotros">Nosotros</a>
-            <a href="/tienda">Tienda</a>
-        </nav>
-        <h1>Nosotros</h1>
-    </body>
-</html>
+@section('contenido')
+Sobre nosotros
+@endsection
